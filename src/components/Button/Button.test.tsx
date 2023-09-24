3028ca1e01aafa9b350e7b1c95182ac64831145b
Cesar Filho <cesar.pc.filho@gmail.com>
2023-09-23 21:33:01 -0300

    chore: create component button

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {fireEvent, render} from '@testing-library/react-native';
+import {StyleSheet} from 'react-native';
+
+import {Button} from '@components/Button';
+
+const {BUTTON_STYLE} = StyleSheet.create({
+  BUTTON_STYLE: {
+    borderRadius: 8,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+describe('Render Button', () => {
+  test('it renders a default button', () => {
+    const title = 'Test Button';
+    const {getByTestId} = render(<Button title={title} />);
+    const component = getByTestId('ComponentButton');
+
+    expect(component).toHaveTextContent(title);
+    expect(component).toHaveStyle(BUTTON_STYLE);
+  });
+
+  test('it calls the onPress callback when clicked', () => {
+    const title = 'Test Button';
+    const click = jest.fn();
+    const {getByTestId} = render(<Button title={title} onPress={click} />);
+    const component = getByTestId('ComponentButton');
+
+    fireEvent.press(component);
+
+    expect(click).toBeCalledTimes(1);
+  });
+});
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,62 @@
 import React from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from 'react-native';
 
-export const Button = () => {
-  return <></>;
-};
+import {useThemeContext} from '@contexts/theme';
+import {Typography} from '@components/Typography';
+import {ThemeMain} from '@theme/index';
+
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+  mode?: 'container' | 'outlined' | 'text';
+  primary?: boolean;
+}
+
+interface StylingProps {
+  colors: typeof ThemeMain;
+  mode?: 'container' | 'outlined' | 'text';
+  primary?: boolean;
+}
+
+export function Button({
+  style,
+  mode = 'container',
+  primary = true,
+  title,
+  ...props
+}: ButtonProps) {
+  const {
+    theme: {colors},
+  } = useThemeContext();
+  const {buttonStyle} = styling({colors, mode, primary});
+
+  return (
+    <TouchableOpacity
+      style={[buttonStyle, style]}
+      activeOpacity={0.8}
+      testID="ComponentButton"
+      {...props}>
+      <Typography variation="button">{title}</Typography>
+    </TouchableOpacity>
+  );
+}
+
+const styling = ({colors, mode, primary}: StylingProps) =>
+  StyleSheet.create({
+    buttonStyle: {
+      borderRadius: 8,
+      height: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      backgroundColor:
+        mode === 'container'
+          ? primary
+            ? colors.primary
+            : colors.secondary
+          : 'transparent',
+    },
+  });
